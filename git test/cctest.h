7b3130a54b67b02dff5908618d00e87ac091e6d6
Edward White <edwardwhite1@outlook.com>
2015-04-20 22:47:00 -0500

    added test class

---
+++ git test/cctest.h
@@ -0,0 +1,13 @@
+//
+//  cctest.h
+//  git test
+//
+//  Created by Edward White on 4/20/15.
+//  Copyright (c) 2015 grinsystems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cctest : NSObject
+
+@end
---
+++ git test/cctest.m
@@ -0,0 +1,13 @@
+//
+//  cctest.m
+//  git test
+//
+//  Created by Edward White on 4/20/15.
+//  Copyright (c) 2015 grinsystems. All rights reserved.
+//
+
+#import "cctest.h"
+
+@implementation cctest
+
+@end
